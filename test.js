a8d41854b633490f760312b437f886c64d3983fb
Aitzaz Haider <aitzaz5@gmail.com>
2018-12-30 06:20:01 -0800

    new comit

--- test.js
+++ test.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-window.alert("Hello World!");
-=======
-window.alert("Hello World!!");
->>>>>>> cd0ef955509330a56a310878cdf1ead20c7fddc7
-=======
-window.alert("Hello World");
->>>>>>> parent of 0c13734... Update test.js
+abc...
